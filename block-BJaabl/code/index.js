9da238f73a5fb868ff1d6b287b01d663614308a9
Bitopan Deka <bitopanxon1@gmail.com>
2021-03-19 15:58:15 -0700

    Refactor

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -28,7 +28,11 @@ addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
 function addTwoNumbers(firstNum, secondNum) {
-  return firstNum + secondNum;
+  if (typeof firstNum !== "number" || typeof secondNum !== "number") {
+    alert("Enter valid Input");
+  } else {
+    return firstNum + secondNum;
+  }
 }
 let addition = addTwoNumbers(12, 34);
 alert(addition);
@@ -44,51 +48,50 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 function calc(numA, numB, operation) {
-  switch (operation) {
-    case "add":
-      return numA + numB;
-      break;
-    case "sub":
-      return numA - numB;
-      break;
-    case "mul":
-      return numA * numB;
-      break;
-    default:
-      alert(`Enter Valid Input`);
+  if (typeof firstNum !== "number" || typeof secondNum !== "number") {
+    alert("Enter valid Input");
+  } else {
+    switch (operation) {
+      case "add":
+        return numA + numB;
+        break;
+      case "sub":
+        return numA - numB;
+        break;
+      case "mul":
+        return numA * numB;
+        break;
+      default:
+        alert(`Enter Valid Input`);
+    }
   }
-}
-let calcu = calc(3, 20, "mul");
-alert(calcu);
-/*
+  let calcu = calc(3, 20, "mul");
+  alert(calcu);
+  /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
 on if the year id leap year or not.
 
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-function isLeapYear(year) {
-  if (year % 4 === 0) {
-    return true;
-  } else if (year % 100 === 0) {
-    return false;
-  } else if (year % 400 === 0) {
-    return true;
-  } else {
-    alert(`enter a valid input`);
+  function isLeapYear(year) {
+    if (year % 4 === 0) return true;
+    if (year % 100 === 0) return false;
+    return year % 4 === 0;
   }
-}
-let lyear = isLeapYear(2001);
-alert(lyear);
-/*
+  let lyear = isLeapYear(2001);
+  alert(lyear);
+  /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
-function getFactorial(numb) {
-  let final = 1;
-  for (let i = numb; i >= 1; i--) {
-    final *= i;
+
+  function getFactorial(numb) {
+    let final = 1;
+    for (let i = numb; i >= 1; i--) {
+      final *= i;
+    }
+    return final;
   }
-  return final;
 }
 let factoial = getFactorial(4);
 alert(factoial);
