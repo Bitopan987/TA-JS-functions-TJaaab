eb37574f428b989a1dd49146944e58331503b19d
Bitopan Deka <bitopanxon1@gmail.com>
2021-03-19 21:37:09 -0700

    Completed Assignment:Function Types

--- block-BJaabr/code/index.js
+++ block-BJaabr/code/index.js
@@ -41,19 +41,25 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function addOne(number) {
+  return number + 1;
+}
 // - Write a Function Expression
-
+let addOne = function (number) {
+  return number + 1;
+};
 // - Write an Arrow Function without curly brackets(if possible)
-
+let addOne = (number) => number + 1;
 // - Write an Arrow Function with curly brackets
-
+let addOne = (number) => {
+  return number + 1;
+};
 // - Execute the function
-
+addOne(21);
 // - Execute the function and store the return value in a variable.
-
+let addedValue = addOne(21);
 // - What is the typeof returnValue
-
+// typeof addedValue "string"
 /* 2. Subtract one
 
 Write a function named `substractOne` that accepts a number, substracts one from the given value and return it. (hint: use return keyword)
@@ -62,19 +68,25 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function substractOne(numb) {
+  return numb - 1;
+}
 // - Write a Function Expression
-
+let substractOne = function (numb) {
+  return numb - 1;
+};
 // - Write an Arrow Function without curly brackets(if possible)
-
+let substractOne = (numb) => numb - 1;
 // - Write an Arrow Function with curly brackets
-
+let substractOne = (numb) => {
+  return numb - 1;
+};
 // - Execute the function
-
+substractOne(21);
 // - Execute the function and store the return value in a variable.
-
+let substractValue = substractOne(21);
 // - What is the typeof returnValue
-
+// typeof substractValue is "string"
 /* 3. Add two numbers
 
 Write a function named `sum` that accepts two numbers and returns the sum of them.
@@ -83,19 +95,25 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function sum(num1, num2) {
+  return num1 + num2;
+}
 // - Write a Function Expression
-
+let sum = function (num1, num2) {
+  return num1 + num2;
+};
 // - Write an Arrow Function without curly brackets(if possible)
-
+let sum = (num1, num2) => num1 + num2;
 // - Write an Arrow Function with curly brackets
-
+let sum = (num1, num2) => {
+  return num1 + num2;
+};
 // - Execute the function
-
+sum(20, 45);
 // - Execute the function and store the return value in a variable
-
+let returnValue = sum(20, 45);
 // - What is the typeof returnValue
-
+// typeof returnValue is "string"
 /* 4. Square of the given value
 
 Write a function named `square` that accepts a number and returns the square of the number.
@@ -104,19 +122,25 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function square(num) {
+  return num * num;
+}
 // - Write a Function Expression
-
+let square = function (num) {
+  return num * num;
+};
 // - Write an Arrow Function without curly brackets(if possible)
-
+let square = (num) => num * num;
 // - Write an Arrow Function with curly brackets
-
+let square = (num) => {
+  return num * num;
+};
 // - Execute the function
-
+square(4);
 // - Execute the function and store the return value in a variable
-
+let returnValue = square(4);
 // - What is the typeof returnValue
-
+// typeof returnValue is "String"
 /* 5. Find isGreater
 
 Write a function named isGreater that accepts two numbers`x` and`y` and returns`true` if x is greater than y or return `false`.
@@ -125,19 +149,25 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function isGreater(x, y) {
+  return x > y;
+}
 // - Write a Function Expression
-
+let isGreater = function (x, y) {
+  return x > y;
+};
 // - Write an Arrow Function without curly brackets(if possible)
-
+let isGreater = (x, y) => x > y;
 // - Write an Arrow Function with curly brackets
-
+let isGreater = (x, y) => {
+  return x > y;
+};
 // - Execute the function
-
+isGreater(10, 5);
 // - Execute the function and store the return value in a variable
-
+let returnValue = isGreater(10, 5);
 // - What is the typeof returnValue
-
+// typeof returnValue is true.
 /* 6.
 
 Write a function named `oddOrEven` that accepts a`number` and returns
@@ -149,17 +179,27 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function oddOrEven(number) {
+  return number % 2 === 0 ? `Number is even` : `Number is odd`;
+}
 // - Write an anonymous Function Expression
-
+let oddOrEven = function (number) {
+  return number % 2 === 0 ? `Number is even` : `Number is odd`;
+};
 // - Write an named Function Expression
-
+let oddOrEven = function isOddOrEven(number) {
+  return number % 2 === 0 ? `Number is even` : `Number is odd`;
+};
 // - Write an Arrow Function without curly brackets (hint: use ternary operator)
-
+let oddOrEven = (number) =>
+  number % 2 === 0 ? `Number is even` : `Number is odd`;
 // - Write an Arrow Function with curly brackets
-
+let oddOrEven = (number) => {
+  return number % 2 === 0 ? `Number is even` : `Number is odd`;
+};
 // - Execute the function
-
+oddOrEven(20);
 // - Execute the function and store the return value in a variable
-
+let returnValue = oddOrEven(20);
 // - What is the typeof returnValue
+// typeof returnValue is "Number is even"
